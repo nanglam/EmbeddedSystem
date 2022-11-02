4dea8132c3aaf4bb4fd14f6c1fdd241a5675d19c
nanglam <nang_lam@naver.com>
2022-11-01 17:40:48 -0700

    add some things...

---
+++ code/change_buffer_size.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<fcntl.h>
+#include<sys/stat.h>
+#include<sys/ioctl.h>
+
+#define DEVICE_FILE_NAME = "/dev/VirtualDisk"
+#define VIR_DEV_MAJOR = 246
+#define WRBUFSIZE _IOW(VIR_DEV_MAJOR, 1, int) 
+#define REBUFSIZE _IOR(VIR_DEV_MAJOR, 2, int)
+
+
+int main(int argc, char **argv) {
+    int device;
+    if(argc != 3) {
+        printf("Usage : ./change_buffer_size [WriteBuffer] [ReadBuffer]")
+    }
+
+    char wbuf[128] = "Write buffer data";
+    char rbuf[128] = "Read buffer data";
+    int n = atoi(argv[1]);
+
+    device = open(DEVICE_FILE_NAME, O_RDWR | O_NDELAY);
+    if (device >= 0) {
+        printf("ioctl is calld: %d\n", n);
+        ioctl(device, WRBUFSIZE, argv[1]);
+        ioctl(device, REBUFSIZE, argv[2]);
+        
+
+        write(device, wbuf, sizeof(wbuf));
+        printf("Write value is %s\n", wbuf);
+
+        read(device, rbuf, sizeof(rbuf));
+        printf("Read valuit is %s\n", rbuf);
+    } else {
+        perror("Device file open fail...\n");
+    }
+
+    close(device);
+    return 0;
+}
--- code/virtual_disk_dd.c
+++ code/virtual_disk_dd.c
@@ -3,3 +3,88 @@
 #include<linux/slab.h>
 #include<linux/fs.h>
 #include<linux/uaccess.h>
+#include<linux/kfifo.h>
+
+#define VIR_DEV_NAME "VirtualDisk"
+#define VIR_DEV_MAJOR 246
+#define BUFFER_SIZE 128
+
+#define WRBUFSIZE _IOW(VIR_DEV_MAJOR, 1, int)
+#define REBUFSIZE _IOR(VIR_DEV_MAJOR, 2, int)
+
+char *internal_buf;
+struct kfifo fifo;
+
+/*
+    unsigned int val;
+    int ret;
+    int i = 0;
+        
+    while(!kfifo_is_full(&fifo)) {
+        kfifo_in(&fifo, &i, sizeof(i));
+        ++i;
+    }
+    while(!kfifo_is_empty(&fifo)) {
+        ret = kfifo_out(&fifo, &val, sizeof(val));
+    }
+
+*/
+
+int vir_open(struct inode *inode, struct file *filep) {
+    int number = MINOR(inode->i_rdev);
+    printk("[CHR_DEV] Virtual Character Device Open: Minor Number: %d\n", number);
+    return 0;
+}
+ssize_t vir_write(struct file *filep, const char *buf, size_t count, loff_t *f_ops) {
+    copy_from_user(internal_buf, buf, count);
+    internal_buf[count] = 0;
+    printk("[CHR_DEV] write data : %s\n", internal_buf);
+    return count;
+}
+ssize_t vir_read(struct file *filep, char *buf, size_t count, loff_t *f_ops) {
+    copy_to_user(buf, internal_buf, count);
+    printk("[CHR_DEV] read data: %s\n", internal_buf);
+    return count;
+}
+long vir_ioctl(struct file *filep, unsigned int cmd, unsigned long arg) {
+    switch (cmd) {
+        case 
+    }
+    return 0;
+}
+int vir_release(struct inode *inode, struct file *filep) {
+    printk("[VirtualDisk] Virtual Disk Device Release\n");
+    return 0;
+}
+struct file_operations vir_fops = {
+    .owner = THIS_MODULE,
+    .write = vir_write,
+    .read = vir_read,
+    .open = vir_open,
+    .release = vir_release,
+    .unlocked_ioctl = vir_ioctl,
+};
+int vir_init(void) {
+    int registration;
+    internal_buf = kmalloc(BUFFER_SIZE, GFP_KERNEL);
+    printk("VirtualDisk201812505 device driver registered\n");
+
+    if(kfifo_alloc(&fifo, 128, GFP_KERNEL))  //init buffer_size : 128
+        printk(KERN_ERR"error kfifo_alloc\n");
+
+    registration = register_chrdev(VIR_DEV_MAJOR, VIR_DEV_NAME, &vir_fops);
+    if(registration < 0)
+        return registration;
+    return 0;
+}
+void vir_exit(void) {
+    printk("VirtualDisk201812505 device driver unregistered\n");
+    kfifo_free(&fifo);
+    Unregistration_chrdev(VIR_DEV_MAJOR, VIR_DEV_NAME)
+}
+
+module_init(vir_init);
+module_exit(vir_exit);
+
+MODULE_LICENSE("GPL");
+
